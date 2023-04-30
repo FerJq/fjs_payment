ad5b56a56e1aacbb7b6d4fb6a4e8e2576419d9df
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-04-30 00:22:56 -0400

    clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { clients } from "../constants";
+import styles from "../style";
 
-const Clients = () => {
-  return <div>Clients</div>;
-};
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          id={client.id}
+          className={`flex-1 flex-center sm:min-w-[192px] min-w-[120px] cursor-pointer hover:brightness-200`}
+        >
+          <img
+            src={client.logo}
+            alt={client}
+            className="sm:w-[192px] w-1[100px]"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
